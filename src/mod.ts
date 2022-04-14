d6577e3fc7f4fd44e0e3d2f3a02e4c6e0827755f
Brad Harris <bharris@slack-corp.com>
2022-04-14 10:45:03 -0600

    adding a SlackFunctionTester to help with writing function tests

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,6 @@
-// TODO: need to work on a more explicit public api for this sdk module
-// Current pass is mostly just exporting everything we want to iterate on
 export { Manifest } from "./manifest.ts";
 export { DefineFunction } from "./functions/mod.ts";
 export { DefineType } from "./types/mod.ts";
 export { default as Schema } from "./schema/mod.ts";
 export { DefineDatastore } from "./datastore/mod.ts";
+export { SlackFunctionTester } from "./functions/function_tester.ts";
