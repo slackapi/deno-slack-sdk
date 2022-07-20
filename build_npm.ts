58b5cc12218eb58562ea430c009d47293d0c26df
Fatma Ekiz <fekiz@slack-corp.com>
2022-07-20 15:12:30 -0700

    changed the package name to slack back

--- build_npm.ts
+++ build_npm.ts
@@ -12,7 +12,7 @@ await build({
   },
   package: {
     // package.json properties
-    name: "test-deno-slack-sdk",
+    name: "@slack/deno-slack-sdk",
     version: Deno.args[0],
     description: "Deno Slack SDK published to NPM to be consumed in node Slack apps",
     license: "MIT",
