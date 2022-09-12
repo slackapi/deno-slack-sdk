e7eac0c867c6b53b782a522f9bc127a29799fc6e
Brad Harris <bharris@slack-corp.com>
2022-09-12 16:50:42 -0600

    using published version of api

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,2 @@
-export { SlackAPI } from "https://raw.githubusercontent.com/slackapi/deno-slack-api/ea242bd21863df1411cb1720116048905ce81e75/src/mod.ts";
-export type { SlackAPIClient } from "https://raw.githubusercontent.com/slackapi/deno-slack-api/ea242bd21863df1411cb1720116048905ce81e75/src/types.ts";
-// export { SlackAPI } from "https://deno.land/x/deno_slack_api@1.0.1/mod.ts";
-// export type { SlackAPIClient } from "https://deno.land/x/deno_slack_api@1.0.1/types.ts";
+export { SlackAPI } from "https://deno.land/x/deno_slack_api@1.1.0/mod.ts";
+export type { SlackAPIClient } from "https://deno.land/x/deno_slack_api@1.1.0/types.ts";
