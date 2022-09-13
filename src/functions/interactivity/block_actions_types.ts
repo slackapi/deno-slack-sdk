c90d4497f5e507b45f38b48d85a7a5ffb799ec9f
Filip Maj <fmaj@slack-corp.com>
2022-09-13 14:40:47 -0400

    First pass at message types on block action body

--- src/functions/interactivity/block_actions_types.ts
+++ src/functions/interactivity/block_actions_types.ts
@@ -47,6 +47,57 @@ export type BlockActionsBody = {
    * @description Whether this event originated from a workspace that is part of an enterprise installation.
    */
   is_enterprise_install: boolean;
+  /**
+   * @description Information about the source message this Block Action interaction originated from.
+   * This may be optional in the case that the Block Action interaction originated from a view rather than a message.
+   */
+  // TODO: maybe factor this into own type once we need to re-use?
+  message?: {
+    /**
+     * @description The encoded application ID the event was dispatched to, e.g. A123456.
+     */
+    app_id: string;
+    /**
+     * @description The Block Kit elements included in the message.
+     */
+    blocks: BlockElement[];
+    /**
+     * @description Message metadata, if any was attached to the message.
+     */
+    metadata?: {
+      event_type: string,
+      event_payload: {
+        // deno-lint-ignore no-explicit-any
+        [key: string]: any;
+      }
+    }
+    /**
+     * @description Encoded team ID, e.g. T123456.
+     */
+    team: string;
+    /**
+     * @description The text in the message. If the message was composed of Block Kit elements, this property would
+     * contain the fallback text to display in constrained UIs (like notifications) or in screenreaders. See
+     * {@link https://api.slack.com/methods/chat.postMessage#blocks_and_attachments the API documentation for use of text, blocks and attachments in messages}.
+     */
+    text: string,
+    /**
+     * @description Timestamp of the parent message. If the message is already a parent message, then this value will equal the `ts` value. Use this value if you want to post a message as a {@link https://api.slack.com/messaging/managing#threading threaded reply} to a particular message.
+     */
+    thread_ts: string;
+    /**
+     * @description Timestamp of the message.
+     */
+    ts: string,
+    /**
+     * @description The type of message. This is always "message."
+     */
+    type: "message",
+    /**
+     * @description Encoded user ID of the user that posted the message, e.g. U123456.
+     */
+    user: string,
+  },
   response_url: string;
   /**
    * @description The workspace, or team, details the Block Kit interaction originated from.
@@ -87,8 +138,7 @@ export type BlockActionsBody = {
   [key: string]: any;
   /* Other properties seen on this type that should be added at some point:
    * container: {}; // should be a reference to message or view below, depending on the container for the block kit interactive componenet
-   * message: {}; // if the block kit interactive componnet was part of a message, details for the message are here
-   * view: {}; // if the block kit interactive componnet was part of a view, details for the view are here
+   * view: {}; // if the block kit interactive component was part of a view, details for the view are here
    * state: { values: {}}; // seen but usually empty?
    */
 };
