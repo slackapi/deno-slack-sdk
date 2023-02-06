9ca9bff25f37f95683909e92c055845b4365ac06
Filip Maj <fmaj@slack-corp.com>
2023-02-06 14:40:07 -0500

    Split out parameter tests for inputs, outputs and input optionality into separate tests. Also add relevant tests for all base primitive types.

---
+++ tests/integration/functions/runtime_context/input_parameter_optionality_test.ts
@@ -0,0 +1,130 @@
+import { assert } from "../../../../src/dev_deps.ts";
+import { CanBe, CanBeUndefined } from "../../../../src/test_utils.ts";
+import { DefineFunction, Schema } from "../../../../src/mod.ts";
+import {
+  EnrichedSlackFunctionHandler,
+} from "../../../../src/functions/types.ts";
+import { SlackFunctionTester } from "../../../../src/functions/tester/mod.ts";
+
+/**
+ * Custom Function handler tests exercising optionality of inputs for primitive types
+ */
+Deno.test("Custom Function with an optional string input provide the string/undefined input in a function handler context", () => {
+  const TestFn = DefineFunction({
+    callback_id: "test",
+    title: "test fn",
+    source_file: "test.ts",
+    input_parameters: {
+      properties: {
+        in: {
+          type: Schema.types.string,
+        },
+      },
+      required: [],
+    },
+  });
+  const handler: EnrichedSlackFunctionHandler<typeof TestFn.definition> = (
+    { inputs },
+  ) => {
+    assert<CanBe<typeof inputs.in, string>>(true);
+    assert<CanBeUndefined<typeof inputs.in>>(true);
+    return {
+      outputs: {
+        out: inputs.in || "default",
+      },
+    };
+  };
+  const { createContext } = SlackFunctionTester(TestFn);
+  const inputs = {};
+  const result = handler(createContext({ inputs }));
+});
+
+Deno.test("Custom Function with an optional boolean input provide the boolean/undefined input in a function handler context", () => {
+  const TestFn = DefineFunction({
+    callback_id: "test",
+    title: "test fn",
+    source_file: "test.ts",
+    input_parameters: {
+      properties: {
+        in: {
+          type: Schema.types.boolean,
+        },
+      },
+      required: [],
+    },
+  });
+  const handler: EnrichedSlackFunctionHandler<typeof TestFn.definition> = (
+    { inputs },
+  ) => {
+    assert<CanBe<typeof inputs.in, boolean>>(true);
+    assert<CanBeUndefined<typeof inputs.in>>(true);
+    return {
+      outputs: {
+        out: inputs.in || "default",
+      },
+    };
+  };
+  const { createContext } = SlackFunctionTester(TestFn);
+  const inputs = {};
+  const result = handler(createContext({ inputs }));
+});
+
+Deno.test("Custom Function with an optional integer input provide the number/undefined input in a function handler context", () => {
+  const TestFn = DefineFunction({
+    callback_id: "test",
+    title: "test fn",
+    source_file: "test.ts",
+    input_parameters: {
+      properties: {
+        in: {
+          type: Schema.types.integer,
+        },
+      },
+      required: [],
+    },
+  });
+  const handler: EnrichedSlackFunctionHandler<typeof TestFn.definition> = (
+    { inputs },
+  ) => {
+    assert<CanBe<typeof inputs.in, number>>(true);
+    assert<CanBeUndefined<typeof inputs.in>>(true);
+    return {
+      outputs: {
+        out: inputs.in || "default",
+      },
+    };
+  };
+  const { createContext } = SlackFunctionTester(TestFn);
+  const inputs = {};
+  const result = handler(createContext({ inputs }));
+});
+
+Deno.test("Custom Function with an optional number input provide the number/undefined input in a function handler context", () => {
+  const TestFn = DefineFunction({
+    callback_id: "test",
+    title: "test fn",
+    source_file: "test.ts",
+    input_parameters: {
+      properties: {
+        in: {
+          type: Schema.types.number,
+        },
+      },
+      required: [],
+    },
+  });
+  const handler: EnrichedSlackFunctionHandler<typeof TestFn.definition> = (
+    { inputs },
+  ) => {
+    assert<CanBe<typeof inputs.in, number>>(true);
+    assert<CanBeUndefined<typeof inputs.in>>(true);
+    return {
+      outputs: {
+        out: inputs.in || "default",
+      },
+    };
+  };
+  const { createContext } = SlackFunctionTester(TestFn);
+  const inputs = {};
+  const result = handler(createContext({ inputs }));
+});
