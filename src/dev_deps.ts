6b78992e07d04717d00e54ade3990cc330622924
William Bergamin <william.bergamin.coen@gmail.com>
2023-02-15 19:55:39 -0500

    Improve built-in generation script and include generated unit tests (#140)

--- src/dev_deps.ts
+++ src/dev_deps.ts
@@ -4,6 +4,7 @@ export {
   assertMatch,
   assertRejects,
   assertStrictEquals,
+  assertStringIncludes,
 } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 export * as mock from "https://deno.land/std@0.152.0/testing/mock.ts";
 
@@ -12,3 +13,5 @@ export type {
   IsAny,
   IsExact,
 } from "https://deno.land/x/conditional_type_checks@1.0.6/mod.ts";
+
+export { pascalCase } from "https://deno.land/x/case@v2.1.0/mod.ts";
---
+++ src/schema/slack/functions/_scripts/src/test/utils_test.ts
@@ -0,0 +1,57 @@
+import {
+  getSlackFunctions,
+  greenText,
+  isArrayFunctionProperty,
+  isObjectFunctionProperty,
+  redText,
+  yellowText,
+} from "../utils.ts";
+import { assert, assertEquals, IsExact } from "../../../../../../dev_deps.ts";
+import {
+  ArrayFunctionProperty,
+  FunctionProperty,
+  ObjectFunctionProperty,
+} from "../types.ts";
+
+Deno.test("colored text remain consistent", () => {
+  assertEquals("\x1b[92mtest\x1b[0m", greenText("test"));
+  assertEquals("\x1b[91mtest\x1b[0m", redText("test"));
+  assertEquals("\x1b[38;5;214mtest\x1b[0m", yellowText("test"));
+});
+
+Deno.test("Non builtin functions should be filtered", async () => {
+  const actual = await getSlackFunctions(
+    "src/schema/slack/functions/_scripts/src/test/data/function.json",
+  );
+  assertEquals(actual.length, 1);
+});
+
+Deno.test("isObjectFunctionProperty distinguishes ObjectFunctionProperty from FunctionProperty", () => {
+  const property: FunctionProperty = {
+    type: "object",
+    properties: {
+      myString: {
+        type: "string",
+        description: "test description",
+        title: "String property",
+      },
+    },
+    required: [],
+    additionalProperties: true,
+  };
+  assert<IsExact<ObjectFunctionProperty, typeof property>>(true);
+  assertEquals(true, isObjectFunctionProperty(property));
+});
+
+Deno.test("isArrayFunctionProperty distinguishes ArrayFunctionProperty from FunctionProperty", () => {
+  const property: FunctionProperty = {
+    type: "array",
+    description: "test description",
+    title: "ArrayFunctionProperty",
+    items: {
+      type: "string",
+    },
+  };
+  assert<IsExact<ArrayFunctionProperty, typeof property>>(true);
+  assertEquals(true, isArrayFunctionProperty(property));
+});
