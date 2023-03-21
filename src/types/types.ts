1c7cb71cf4d6c75b6a8669cf0e15333692ee3dec
William Bergamin <wbergamin@slack-corp.com>
2023-03-21 13:19:15 -0400

    Cleaned up code

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,16 +5,7 @@ import {
 } from "../parameters/definition_types.ts";
 import { SlackManifest } from "../manifest/mod.ts";
 import { ManifestCustomTypeSchema } from "../manifest/manifest_schema.ts";
-import { CustomType } from "./mod.ts";
 
-type TypedCustomType = CustomType<
-  TypedObjectProperties,
-  TypedObjectRequiredProperties<TypedObjectProperties>,
-  CustomTypeDefinition<
-    TypedObjectProperties,
-    TypedObjectRequiredProperties<TypedObjectProperties>
-  >
->;
 export type CustomTypeDefinition<
   Props extends TypedObjectProperties,
   RequiredProps extends TypedObjectRequiredProperties<Props>,
