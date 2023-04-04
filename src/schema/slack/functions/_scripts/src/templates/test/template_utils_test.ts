3d60026b1cda8fc0c573fd5acd96ad68d3ad649b
William Bergamin <william.bergamin.coen@gmail.com>
2023-04-04 16:18:59 -0400

    Add recursive type investigation for imports (#158)
    * Recusivly look through function for types
    
    * Improve code coverage
    
    * Added more tests
    
    * test with primitive parameter, object parameter, array parameter

--- src/schema/slack/functions/_scripts/src/templates/test/template_utils_test.ts
+++ src/schema/slack/functions/_scripts/src/templates/test/template_utils_test.ts
@@ -46,7 +46,7 @@ Deno.test("getSlackCallbackId should generate the valid slack callback_id", () =
   assertStringIncludes(actual, expected);
 });
 
-Deno.test("renderTypeImports should render all imports provided", () => {
+Deno.test("renderTypeImports should render all imports provided with slack and primitive types", () => {
   const dfi: FunctionRecord = {
     callback_id: CALLBACK_ID,
     title: TITLE,
@@ -75,13 +75,29 @@ Deno.test("renderTypeImports should render all imports provided", () => {
         title: "Message time stamp",
         description: "Message time stamp",
       },
+    ],
+  };
+  const actual = renderTypeImports(dfi);
+  assertStringIncludes(actual, "SchemaTypes");
+  assertStringIncludes(actual, "SlackTypes");
+  assertStringIncludes(actual, "InternalSlackTypes");
+});
+
+Deno.test("renderTypeImports should render imports required for array type", () => {
+  const dfi: FunctionRecord = {
+    callback_id: CALLBACK_ID,
+    title: TITLE,
+    description: DESCRIPTION,
+    type: "builtin",
+    input_parameters: [],
+    output_parameters: [
       {
         type: SchemaTypes.array,
         name: "user_ids",
         title: "User Ids",
         description: "User Ids",
         items: {
-          type: "integer",
+          type: SlackTypes.channel_id,
         },
       },
     ],
@@ -89,5 +105,95 @@ Deno.test("renderTypeImports should render all imports provided", () => {
   const actual = renderTypeImports(dfi);
   assertStringIncludes(actual, "SchemaTypes");
   assertStringIncludes(actual, "SlackTypes");
+});
+
+Deno.test("renderTypeImports should render imports required for object type", () => {
+  const dfi: FunctionRecord = {
+    callback_id: CALLBACK_ID,
+    title: TITLE,
+    description: DESCRIPTION,
+    type: "builtin",
+    input_parameters: [],
+    output_parameters: [
+      {
+        type: SchemaTypes.object,
+        name: "user_ids",
+        title: "User Ids",
+        description: "User Ids",
+        properties: {
+          my_param: {
+            type: SlackTypes.channel_id,
+          },
+        },
+      },
+    ],
+  };
+  const actual = renderTypeImports(dfi);
+  assertStringIncludes(actual, "SchemaTypes");
+  assertStringIncludes(actual, "SlackTypes");
+});
+
+Deno.test("renderTypeImports should render imports required for a nested complex object type", () => {
+  const dfi: FunctionRecord = {
+    callback_id: CALLBACK_ID,
+    title: TITLE,
+    description: DESCRIPTION,
+    type: "builtin",
+    input_parameters: [],
+    output_parameters: [
+      {
+        type: SchemaTypes.array,
+        items: {
+          type: SchemaTypes.object,
+          properties: {
+            my_slack_type: {
+              type: InternalSlackTypes.form_input_object.id,
+            },
+            my_primitive_type: {
+              type: SlackTypes.channel_id,
+            },
+          },
+        },
+        name: "user_ids",
+      },
+    ],
+  };
+  const actual = renderTypeImports(dfi);
   assertStringIncludes(actual, "InternalSlackTypes");
+  assertStringIncludes(actual, "SchemaTypes");
+  assertStringIncludes(actual, "SlackTypes");
+});
+
+Deno.test("renderTypeImports should render imports required for primitive & complex types", () => {
+  const dfi: FunctionRecord = {
+    callback_id: CALLBACK_ID,
+    title: TITLE,
+    description: DESCRIPTION,
+    type: "builtin",
+    input_parameters: [],
+    output_parameters: [
+      {
+        type: SchemaTypes.array,
+        items: {
+          type: SchemaTypes.string,
+        },
+        name: "user_ids",
+      },
+      {
+        type: SchemaTypes.object,
+        name: "my_object",
+        properties: {
+          my_param: {
+            type: SchemaTypes.string,
+          },
+        },
+      },
+      {
+        type: SchemaTypes.string,
+        name: "my_primitive",
+      },
+    ],
+  };
+  const actual = renderTypeImports(dfi);
+  assertStringIncludes(actual, "SchemaTypes");
 });
