0a615d6a49234118ab9d9c439dea241072dbe287
William Bergamin <wbergamin@slack-corp.com>
2023-09-06 14:50:34 -0400

    Revert "update"
    This reverts commit 533995833c45b6d3c39d3401a99f4f111179c005.

--- src/parameters/definition_types_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertExists } from "../dev_deps.ts";
-import * as definitionTypes from "./definition_types.ts";
-
-Deno.test("Include definition-types.ts in code coverage", () => {
-  assertExists(definitionTypes);
-});
--- src/test_utils_test.ts
+++
@@ -1,7 +0,0 @@
-import { assertExists } from "./dev_deps.ts";
-import * as test_utils from "./test_utils.ts";
-
-// TODO: add tests here
-Deno.test("Include test_utils.ts in code coverage", () => {
-  assertExists(test_utils);
-});
