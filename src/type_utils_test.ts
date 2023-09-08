3fed1c5a2af0393a3cbcba0396dbeb8cb6631ea9
William Bergamin <wbergamin@slack-corp.com>
2023-09-08 17:19:12 -0400

    Delete type_utils_test.ts

--- src/type_utils_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertExists } from "./dev_deps.ts";
-import * as typeUtils from "./type_utils.ts";
-
-Deno.test("Include type_utils.ts in code coverage", () => {
-  assertExists(typeUtils);
-});
