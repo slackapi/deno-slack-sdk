d210a4e6a0d03829808424fb1bc037006f51b665
William Bergamin <wbergamin@slack-corp.com>
2023-09-20 12:40:21 -0400

    Improve the builtin generation tests

--- tests/integration/schema/slack/functions/_scripts/write_function_files_test.ts
+++ tests/integration/schema/slack/functions/_scripts/write_function_files_test.ts
@@ -1,8 +1,12 @@
-import { assertSnapshot, mock } from "../../../../../../src/dev_deps.ts";
+import {
+  assertEquals,
+  assertExists,
+  mock,
+} from "../../../../../../src/dev_deps.ts";
 import { _internals } from "../../../../../../src/schema/slack/functions/_scripts/src/write_function_files.ts";
 import { getSlackFunctions } from "../../../../../../src/schema/slack/functions/_scripts/src/utils.ts";
 
-Deno.test(`write_function_files.ts ${_internals.main.name} function snapshot test`, async (t) => {
+Deno.test(`write_function_files.ts ${_internals.main.name} function generates valid typescript`, async () => {
   const getSlackFunctionsStub = mock.stub(
     _internals,
     "getSlackFunctions",
@@ -16,16 +20,14 @@ Deno.test(`write_function_files.ts ${_internals.main.name} function snapshot tes
     },
   );
 
-  const paths: string[] = [];
-  const fileContents: string[] = [];
+  const outputs: Record<string, string> = {};
 
   const writeTextFileStub = mock.stub(
     _internals,
     "writeTextFile",
     // deno-lint-ignore require-await
     async (path, data) => {
-      paths.push(path);
-      fileContents.push(data);
+      outputs[path] = data;
     },
   );
 
@@ -33,8 +35,31 @@ Deno.test(`write_function_files.ts ${_internals.main.name} function snapshot tes
     await _internals.main();
     mock.assertSpyCalls(writeTextFileStub, 3);
     mock.assertSpyCalls(getSlackFunctionsStub, 1);
-    await assertSnapshot(t, paths);
-    await assertSnapshot(t, fileContents);
+
+    const generatedContent = outputs["../send_message.ts"];
+    assertExists(generatedContent);
+
+    const command = new Deno.Command(Deno.execPath(), {
+      cwd: `${Deno.cwd()}/src/schema/slack/functions/`,
+      args: [
+        "eval",
+        generatedContent,
+      ],
+    });
+    const { code, stdout, stderr } = await command.output();
+    const textDecoder = new TextDecoder();
+
+    assertEquals(
+      textDecoder.decode(stderr),
+      "",
+      "The generated TypeScript content is not valid",
+    );
+    assertEquals(
+      textDecoder.decode(stdout),
+      "",
+      "The generated TypeScript should not print to the console",
+    );
+    assertEquals(code, 0);
   } finally {
     writeTextFileStub.restore();
     getSlackFunctionsStub.restore();
