5e28766eed530632468503341caf610068c1773f
William Bergamin <wbergamin@slack-corp.com>
2023-10-04 15:02:44 -0400

    Add tests

--- src/mod_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertExists } from "./dev_deps.ts";
-import * as mod from "./mod.ts";
-
-Deno.test("Include all content of mod.ts in code coverage", () => {
-  assertExists(mod);
-});
--- src/schema/slack/functions/_scripts/src/write_function_files_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertExists } from "../../../../../dev_deps.ts";
-import * as writeFunctionFiles from "./write_function_files.ts";
-
-Deno.test("Include all content of generation script in code coverage", () => {
-  assertExists(writeFunctionFiles);
-});
