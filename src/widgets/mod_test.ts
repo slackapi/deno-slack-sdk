88ffa0ff89a76fef82d7b1cb2e04fa05d46a64a0
Chris Roberson <croberson@slack-corp.com>
2023-12-01 16:43:40 -0500

    Add another test

---
+++ src/widgets/mod_test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "../dev_deps.ts";
+import { DefineWidget } from "./mod.ts";
+import { SlackWidgetDataMode } from "./types.ts";
+
+Deno.test("Widget exports json as expected", () => {
+  const widgetSerialized = {
+    title: "test",
+    description: "this widget",
+    workflow_id: "1",
+    data_mode: SlackWidgetDataMode.PER_INSTALLER,
+  };
+  const widget = DefineWidget({
+    callback_id: "test",
+    ...widgetSerialized,
+  });
+
+  const json = widget.export();
+  assertEquals(json, widgetSerialized);
+});
