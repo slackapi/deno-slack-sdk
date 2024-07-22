9cea9516e7073cce79e4147ffd5e863223893dde
Filip Maj <fmaj@slack-corp.com>
2024-07-22 11:14:58 -0400

    Bump deno_slack_api to 2.7.1

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export { SlackAPI } from "https://deno.land/x/deno_slack_api@2.7.0/mod.ts";
+export { SlackAPI } from "https://deno.land/x/deno_slack_api@2.7.1/mod.ts";
 export type {
   SlackAPIClient,
   Trigger,
-} from "https://deno.land/x/deno_slack_api@2.7.0/types.ts";
+} from "https://deno.land/x/deno_slack_api@2.7.1/types.ts";
